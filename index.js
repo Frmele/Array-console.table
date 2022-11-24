0b26026a45ef644ac713d5171b981d857e2979b5
Frmele <76843521+Frmele@users.noreply.github.com>
2022-11-24 20:23:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+const thingsToDiscuss = ["colors", "who does what", "budget", "sand paper"];
+
+// Shows the array in a table with a column for the indexes
+console.table(thingsToDiscuss);
